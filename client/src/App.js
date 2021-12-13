a3bc93d1d7f141d0990a8bb9bbfa968fc825d39f
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2021-12-13 15:44:52 -0600

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link, Route, Switch } from 'react-router-dom'; 
 
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
